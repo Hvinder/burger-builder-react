e3761ea8e37d49229d4569dcf035e8a08c5fca0a
Hvinder <harvinder.chavan7@gmail.com>
2021-02-07 16:15:38 +0530

    Improved performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = (props) => (
-  <>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh",
-        opacity: props.show ? "1" : "0",
-      }}
-    >
-      {props.children}
-    </div>
-  </>
-);
+const Modal = (props) => {
+  useEffect(() => {}, [props.show]);
+  return (
+    <>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh",
+          opacity: props.show ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
 
 export default Modal;
